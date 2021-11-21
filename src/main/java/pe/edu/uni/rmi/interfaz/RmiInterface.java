7eb0a4f3848afe0bf868d9bf023715aff78325b6
rmartinezch <rmartinezch@gmail.com>
2021-11-20 20:08:51 -0500

    Create RMI Interface

---
+++ src/main/java/pe/edu/uni/rmi/interfaz/RmiInterface.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pe.edu.uni.rmi.interfaz;
+
+/**
+ *
+ * @author Intel
+ */
+public interface RmiInterface {
+    public int suma(int a, int b);
+    public int diferencia(int a, int b);
+    public int producto(int a, int b);
+    public int cociente(int a, int b);
+}
